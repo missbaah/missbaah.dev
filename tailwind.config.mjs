f041659777cad023fb54c6a86f8cb81936ff4ecc
Adwoa Baah Addo-Brako <adwoaaddo-brako@Adwoas-MacBook-Pro.local>
2025-01-08 18:13:40 +0000

    added icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    iconsPlugin({
+      collections: getIconCollections(["lucide"]),
+    }),
+  ],
+};
