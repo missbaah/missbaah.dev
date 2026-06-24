0337e2809665897a394105a7c59fd65bc0a0927c
Adwoa Baah Addo-Brako <baahbrako@gmail.com>
2026-06-24 13:59:41 +0000

    feat: add project data and update ProjectCard component; enhance project display on pages

---
+++ src/data/project.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: "Project One",
+    description: "A short description of what this project does.",
+    imgsrc: "/image1.png",
+    featured: true,
+  },
+  {
+    title: "Project Two",
+    description: "A short description of what this project does.",
+    imgsrc: "/image1.png",
+    featured: false,
+  },
+  {
+    title: "Project Three",
+    description: "A short description of what this project does.",
+    imgsrc: "/image1.png",
+    featured: false,
+  },
+]
