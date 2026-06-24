f306e356b84663852d0c1d6cbb1cf40aa92bfc36
Adwoa Baah Addo-Brako <baahbrako@gmail.com>
2026-06-24 00:37:20 +0000

    chore: update dependencies and add global styles
    - Removed unused dependency "@astrojs/tailwind" from package.json
    - Added "@tailwindcss/vite", "prettier", "prettier-plugin-astro", and updated "tailwindcss" version in devDependencies
    - Adjusted Nav component to change bottom padding for active links
    - Modified Layout component to remove unnecessary styles and adjust body gap
    - Imported global styles in about, blog, index, notes, and projects pages
    - Created a new global.css file to import Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   fonts: [
     {
       provider: fontProviders.local(),
